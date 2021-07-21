71dab1f323c14f451e34fa29b1f4d30ec12081a3
Susana Martins <scmartins87@gmail.com>
2021-07-21 10:59:32 -0700

    install & import google fonts for oswald and lato, function declared in restaurant-info-card

--- App.js
+++ App.js
@@ -2,6 +2,22 @@ import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
 
+import { useFonts, Oswald_400Regular, } from '@expo-google-fonts/oswald';
+import { useFonts, Lato_400Regular, } from '@expo-google-fonts/lato';
+
+export default function App () {
+  const [oswaldLoaded] = useFonts({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useFonts({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded) || !latoLoaded) {
+    return null;
+  }
+
 import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
